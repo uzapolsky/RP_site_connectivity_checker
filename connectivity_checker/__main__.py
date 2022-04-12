049a57f254be4cb2221247916e1905d23474bae7
uzapolsky <uzapolsky@gmail.com>
2022-04-12 12:22:53 +0500

    initial commit

---
+++ connectivity_checker/__main__.py
@@ -0,0 +1,4 @@
+from checker import site_is_online
+
+if __name__ == '__main__':
+    site_is_online('pypi.org')
---
+++ connectivity_checker/checker.py
@@ -0,0 +1,18 @@
+from http.client import HTTPConnection
+from urllib.parse import urlparse
+
+def site_is_online(url, timeout=2):
+
+    error = Exception("unknown error")
+    parser = urlparse(url)
+    host = parser.netloc or parser.path.split("/")[0]
+    for port in (80, 443):
+        connection = HTTPConnection(host=host, port=port, timeout=timeout)
+        try:
+            connection.request("HEAD", "/")
+            return True
+        except Exception as e:
+            error = e
+        finally:
+            connection.close()
+    raise error
